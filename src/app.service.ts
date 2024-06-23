1e9ca436c8bff46ab34aa88a7f55ed79bc49767a
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-06-23 17:39:50 -0500

    feat: test env

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Hello World!';
+    const PORT = process.env.NEST_PORT;
+    return PORT;
   }
 }
