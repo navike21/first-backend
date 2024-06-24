e4651c1a6aac30ac1f65a01c45562936fe37839d
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-06-23 21:05:04 -0500

    feat: delete portfolio resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,14 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
-import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
-    PortfolioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
