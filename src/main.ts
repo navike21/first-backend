e9ee0ab99a514926489b2042b23267b5236a0b57
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-06-23 21:14:10 -0500

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('First')
+    .setDescription('API First to navike21 project with NestJS')
+    .setVersion('1.0')
+    .addTag('projects')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('documentation', app, document);
+
   await app.listen(5000);
 }
 bootstrap();
