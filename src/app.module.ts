698e4703835a595baf095394de0eb57683b06ebb
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-06-23 21:07:07 -0500

    feat: add projects module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { ProjectsModule } from './projects/projects.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    ProjectsModule,
   ],
   controllers: [AppController],
   providers: [AppService],
