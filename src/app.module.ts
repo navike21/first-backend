11a1efcd002918e72677ddfbf80206140d18ab51
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-06-23 20:57:19 -0500

    feat: add module to collections

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
+import { PortfolioModule } from './portfolio/portfolio.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: '.env',
     }),
+    PortfolioModule,
   ],
   controllers: [AppController],
   providers: [AppService],
