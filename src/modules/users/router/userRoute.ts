519d103ef06e6dc979581920963b5fb26332a2d0
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-08-24 18:59:18 -0500

    feat: add update user controller

--- src/modules/users/router/userRoute.ts
+++ src/modules/users/router/userRoute.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
-import { createUser, listUsers } from '../controllers'
+import { createUser, listUsers, updateUser } from '../controllers'
 import { validateUser } from '../middlewares'
 
 export function users(router: Router) {
   router.post('/users', validateUser, createUser)
-  router.post('/users/all', listUsers)
+  router.post('/users/list-filter', listUsers)
+  router.post('/users/update/:idUser', updateUser)
 }
