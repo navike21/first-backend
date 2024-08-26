c476cda5cf87db599e8796a0106c0b9208c1f213
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-08-25 20:29:49 -0500

    feat: Remove unused code related to users and user groups

--- src/modules/usersGroups/controllers/crud/deleteUserGroup.ts
+++
@@ -1,9 +0,0 @@
-import { getInfoHeaders, IRequest, TRequest, TResponse } from '../../../../common'
-
-export const deleteUserGroup = async (
-  { body, headers }: TRequest,
-  response: TResponse
-) => {
-  const { lang } = getInfoHeaders(headers)
-  const { data } = body as IRequest
-}
