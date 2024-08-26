ba150ab3621be30f26ba051b9cb22b2ffe4405d1
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-08-25 20:35:41 -0500

    refactor: Remove unused code related to demos module

--- src/modules/demos/controllers/crud/createDemo.ts
+++
@@ -1,9 +0,0 @@
-import { getInfoHeaders, IRequest, TRequest, TResponse } from '../../../../common'
-
-export const createDemo = async (
-  { body, headers }: TRequest,
-  response: TResponse
-) => {
-  const { lang } = getInfoHeaders(headers)
-  const { data } = body as IRequest
-}
--- src/modules/demos/schemas/demo.schema.ts
+++
@@ -1,6 +0,0 @@
-import joi from 'joi'
-import { EN, TLanguage } from '../../../common'
-
-export const DemoSchema = (lang: TLanguage = EN) => {
-  return joi.object({})
-}
