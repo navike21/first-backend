804cdef033a403fe9fb45dd60080420823a7b6b7
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-11-01 23:47:38 -0500

    refactor: Update dependencies and enhance error handling in file upload controller

--- src/modules/upload/controllers/uploadFiles.ts
+++ src/modules/upload/controllers/uploadFiles.ts
@@ -1,13 +1,28 @@
-import { generateId, TRequest, TResponse } from '../../../common'
+import {
+  generateId,
+  handleErrors,
+  handleSuccess,
+  TRequest,
+  TResponse
+} from '../../../common'
 import { FileModel } from '../models'
 import { IExtendedFile, IFile } from '../types'
 
-export const uploadFiles = async (req: TRequest, res: TResponse) => {
+export async function uploadFiles(
+  req: TRequest,
+  res: TResponse
+): Promise<void> {
   const files = req.files as IExtendedFile[]
 
   try {
     if (!files || files.length === 0) {
-      return res.status(400).json({ message: 'No files uploaded.' })
+      return handleErrors(
+        {
+          message: 'No files to upload',
+          statusCode: 400
+        },
+        res
+      )
     }
 
     const savedFiles = await Promise.all(
@@ -28,9 +43,9 @@ export const uploadFiles = async (req: TRequest, res: TResponse) => {
       })
     )
 
-    res.status(201).json({ data: savedFiles })
+    handleSuccess({ message: '', statusCode: 201, data: savedFiles }, res)
   } catch (error) {
     const { message } = error as Error
-    res.status(500).json({ message: 'Error uploading files', error: message })
+    handleErrors({ message, statusCode: 500, data: error }, res)
   }
 }
