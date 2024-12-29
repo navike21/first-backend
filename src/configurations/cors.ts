8012a05637e296daf2227d8af40d10f708375e08
José Iván Chapoñán Casiano <ivan@navike21.com>
2024-12-29 02:45:18 -0500

    feat: Enhance CORS configuration to validate origins and specify allowed methods and headers

--- src/configurations/cors.ts
+++ src/configurations/cors.ts
@@ -3,9 +3,19 @@ import { app } from './app'
 import { URL_ORIGINS } from '../common'
 
 export function corsConfig(): void {
+  const allowedOrigins = URL_ORIGINS.split(',')
+
   app.use(
     cors({
-      origin: `${URL_ORIGINS}`.split(',')
+      origin: (origin, callback) => {
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true) // Permite el acceso
+        } else {
+          callback(new Error('Not allowed by CORS')) // Bloquea el acceso
+        }
+      },
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+      allowedHeaders: ['Content-Type', 'Authorization']
     })
   )
 }
