1c85ce3c845cde60be9c2cdc8c988781fb98763a
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-02-11 00:12:46 -0500

    refactor(server): move app configuration into server file and remove separate app module

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,41 @@
-import { app, configureApp } from '@Config/app';
+import express from 'express';
 import { connectToDatabase } from '@Connection/dataBase';
 import mainRouter from '@Routes/route';
 import { errorMiddleware } from '@Middlewares/errorMiddleware';
+import { corsConfig } from '@Config/cors';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import helmet from 'helmet';
+import { globalLimiter } from '@Config/limiter';
 
-configureApp();
+export const app = express();
+
+app.use(corsConfig);
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+	compression({
+		threshold: 1024,
+	}),
+);
+
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+		crossOriginEmbedderPolicy: false,
+		frameguard: { action: 'deny' },
+		referrerPolicy: { policy: 'no-referrer' },
+		hidePoweredBy: true,
+		noSniff: true,
+	}),
+);
+
+app.use(globalLimiter);
 
 connectToDatabase();
 
