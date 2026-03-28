37bd505537d35b1f9077843e80bcfe2a893719c3
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-03-28 04:40:36 -0500

    feat: enhance server setup with middleware and graceful shutdown handling

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,37 @@
-import { startServer } from '@Config/mainServer';
+import { handleServerShutdown, startServer } from '@Config/mainServer';
+import compression from 'compression';
+import cookieParser from 'cookie-parser';
+import helmet from 'helmet';
 import express from 'express';
+import { globalLimiter } from '@Config/limiter';
 
 const app = express();
 
 // Middleware
 app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cookieParser());
+
+app.use(
+	compression({
+		threshold: 1024,
+	}),
+);
+
+app.use(
+	helmet({
+		contentSecurityPolicy: false,
+		crossOriginEmbedderPolicy: false,
+		frameguard: { action: 'deny' },
+		referrerPolicy: { policy: 'no-referrer' },
+		hidePoweredBy: true,
+		noSniff: true,
+	}),
+);
+
+app.use(globalLimiter);
 
 // Define your routes
 app.get('/', (req, res) => {
@@ -16,6 +43,8 @@ app.get('/api/health', (req, res) => {
 });
 
 startServer(app);
+process.on('SIGINT', handleServerShutdown);
+process.on('SIGTERM', handleServerShutdown);
 
 // Export for Vercel serverless
 export default app;
