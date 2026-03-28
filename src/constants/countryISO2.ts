72aa3e05d5fc1fd96449f3af965fb153a64c69db
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-03-28 05:16:43 -0500

    feat: add constants and types for various industries, client types, country ISO codes, status registers, tax identifiers, user genders, and roles

---
+++ src/constants/countryISO2.ts
@@ -0,0 +1,240 @@
+export const COUNTRY_PE = 'PE';
+export const COUNTRY_MX = 'MX';
+export const COUNTRY_CO = 'CO';
+export const COUNTRY_CL = 'CL';
+export const COUNTRY_AR = 'AR';
+export const COUNTRY_BR = 'BR';
+export const COUNTRY_EC = 'EC';
+export const COUNTRY_BO = 'BO';
+export const COUNTRY_PY = 'PY';
+export const COUNTRY_UY = 'UY';
+export const COUNTRY_VE = 'VE';
+export const COUNTRY_PA = 'PA';
+export const COUNTRY_CR = 'CR';
+export const COUNTRY_SV = 'SV';
+export const COUNTRY_GT = 'GT';
+export const COUNTRY_HN = 'HN';
+export const COUNTRY_NI = 'NI';
+export const COUNTRY_DO = 'DO';
+export const COUNTRY_US = 'US';
+export const COUNTRY_CA = 'CA';
+
+export const COUNTRY_ES = 'ES';
+export const COUNTRY_FR = 'FR';
+export const COUNTRY_DE = 'DE';
+export const COUNTRY_IT = 'IT';
+export const COUNTRY_PT = 'PT';
+export const COUNTRY_NL = 'NL';
+export const COUNTRY_BE = 'BE';
+export const COUNTRY_AT = 'AT';
+export const COUNTRY_CH = 'CH';
+export const COUNTRY_SE = 'SE';
+export const COUNTRY_NO = 'NO';
+export const COUNTRY_DK = 'DK';
+export const COUNTRY_FI = 'FI';
+export const COUNTRY_IE = 'IE';
+export const COUNTRY_UK = 'UK';
+export const COUNTRY_PL = 'PL';
+export const COUNTRY_CZ = 'CZ';
+export const COUNTRY_SK = 'SK';
+export const COUNTRY_HU = 'HU';
+export const COUNTRY_RO = 'RO';
+export const COUNTRY_BG = 'BG';
+export const COUNTRY_GR = 'GR';
+
+export const COUNTRY_JP = 'JP';
+export const COUNTRY_CN = 'CN';
+export const COUNTRY_KR = 'KR';
+export const COUNTRY_IN = 'IN';
+export const COUNTRY_SG = 'SG';
+export const COUNTRY_HK = 'HK';
+export const COUNTRY_TW = 'TW';
+export const COUNTRY_TH = 'TH';
+export const COUNTRY_MY = 'MY';
+export const COUNTRY_ID = 'ID';
+export const COUNTRY_PH = 'PH';
+export const COUNTRY_VN = 'VN';
+export const COUNTRY_AE = 'AE';
+export const COUNTRY_SA = 'SA';
+export const COUNTRY_IL = 'IL';
+
+export type CountryISO2 =
+	| typeof COUNTRY_PE
+	| typeof COUNTRY_MX
+	| typeof COUNTRY_CO
+	| typeof COUNTRY_CL
+	| typeof COUNTRY_AR
+	| typeof COUNTRY_BR
+	| typeof COUNTRY_EC
+	| typeof COUNTRY_BO
+	| typeof COUNTRY_PY
+	| typeof COUNTRY_UY
+	| typeof COUNTRY_VE
+	| typeof COUNTRY_PA
+	| typeof COUNTRY_CR
+	| typeof COUNTRY_SV
+	| typeof COUNTRY_GT
+	| typeof COUNTRY_HN
+	| typeof COUNTRY_NI
+	| typeof COUNTRY_DO
+	| typeof COUNTRY_US
+	| typeof COUNTRY_CA
+	| typeof COUNTRY_ES
+	| typeof COUNTRY_FR
+	| typeof COUNTRY_DE
+	| typeof COUNTRY_IT
+	| typeof COUNTRY_PT
+	| typeof COUNTRY_NL
+	| typeof COUNTRY_BE
+	| typeof COUNTRY_AT
+	| typeof COUNTRY_CH
+	| typeof COUNTRY_SE
+	| typeof COUNTRY_NO
+	| typeof COUNTRY_DK
+	| typeof COUNTRY_FI
+	| typeof COUNTRY_IE
+	| typeof COUNTRY_UK
+	| typeof COUNTRY_PL
+	| typeof COUNTRY_CZ
+	| typeof COUNTRY_SK
+	| typeof COUNTRY_HU
+	| typeof COUNTRY_RO
+	| typeof COUNTRY_BG
+	| typeof COUNTRY_GR
+	| typeof COUNTRY_JP
+	| typeof COUNTRY_CN
+	| typeof COUNTRY_KR
+	| typeof COUNTRY_IN
+	| typeof COUNTRY_SG
+	| typeof COUNTRY_HK
+	| typeof COUNTRY_TW
+	| typeof COUNTRY_TH
+	| typeof COUNTRY_MY
+	| typeof COUNTRY_ID
+	| typeof COUNTRY_PH
+	| typeof COUNTRY_VN
+	| typeof COUNTRY_AE
+	| typeof COUNTRY_SA
+	| typeof COUNTRY_IL;
+
+export const COUNTRY_ISO2_ARRAY: CountryISO2[] = [
+	COUNTRY_PE,
+	COUNTRY_MX,
+	COUNTRY_CO,
+	COUNTRY_CL,
+	COUNTRY_AR,
+	COUNTRY_BR,
+	COUNTRY_EC,
+	COUNTRY_BO,
+	COUNTRY_PY,
+	COUNTRY_UY,
+	COUNTRY_VE,
+	COUNTRY_PA,
+	COUNTRY_CR,
+	COUNTRY_SV,
+	COUNTRY_GT,
+	COUNTRY_HN,
+	COUNTRY_NI,
+	COUNTRY_DO,
+	COUNTRY_US,
+	COUNTRY_CA,
+	COUNTRY_ES,
+	COUNTRY_FR,
+	COUNTRY_DE,
+	COUNTRY_IT,
+	COUNTRY_PT,
+	COUNTRY_NL,
+	COUNTRY_BE,
+	COUNTRY_AT,
+	COUNTRY_CH,
+	COUNTRY_SE,
+	COUNTRY_NO,
+	COUNTRY_DK,
+	COUNTRY_FI,
+	COUNTRY_IE,
+	COUNTRY_UK,
+	COUNTRY_PL,
+	COUNTRY_CZ,
+	COUNTRY_SK,
+	COUNTRY_HU,
+	COUNTRY_RO,
+	COUNTRY_BG,
+	COUNTRY_GR,
+	COUNTRY_JP,
+	COUNTRY_CN,
+	COUNTRY_KR,
+	COUNTRY_IN,
+	COUNTRY_SG,
+	COUNTRY_HK,
+	COUNTRY_TW,
+	COUNTRY_TH,
+	COUNTRY_MY,
+	COUNTRY_ID,
+	COUNTRY_PH,
+	COUNTRY_VN,
+	COUNTRY_AE,
+	COUNTRY_SA,
+	COUNTRY_IL,
+];
+
+export const COUNTRY_LIST: Record<CountryISO2, string> = {
+	[COUNTRY_PE]: 'Peru',
+	[COUNTRY_MX]: 'Mexico',
+	[COUNTRY_CO]: 'Colombia',
+	[COUNTRY_CL]: 'Chile',
+	[COUNTRY_AR]: 'Argentina',
+	[COUNTRY_BR]: 'Brazil',
+	[COUNTRY_EC]: 'Ecuador',
+	[COUNTRY_BO]: 'Bolivia',
+	[COUNTRY_PY]: 'Paraguay',
+	[COUNTRY_UY]: 'Uruguay',
+	[COUNTRY_VE]: 'Venezuela',
+	[COUNTRY_PA]: 'Panama',
+	[COUNTRY_CR]: 'Costa Rica',
+	[COUNTRY_SV]: 'El Salvador',
+	[COUNTRY_GT]: 'Guatemala',
+	[COUNTRY_HN]: 'Honduras',
+	[COUNTRY_NI]: 'Nicaragua',
+	[COUNTRY_DO]: 'Dominican Republic',
+	[COUNTRY_US]: 'United States',
+	[COUNTRY_CA]: 'Canada',
+
+	[COUNTRY_ES]: 'Spain',
+	[COUNTRY_FR]: 'France',
+	[COUNTRY_DE]: 'Germany',
+	[COUNTRY_IT]: 'Italy',
+	[COUNTRY_PT]: 'Portugal',
+	[COUNTRY_NL]: 'Netherlands',
+	[COUNTRY_BE]: 'Belgium',
+	[COUNTRY_AT]: 'Austria',
+	[COUNTRY_CH]: 'Switzerland',
+	[COUNTRY_SE]: 'Sweden',
+	[COUNTRY_NO]: 'Norway',
+	[COUNTRY_DK]: 'Denmark',
+	[COUNTRY_FI]: 'Finland',
+	[COUNTRY_IE]: 'Ireland',
+	[COUNTRY_UK]: 'United Kingdom',
+	[COUNTRY_PL]: 'Poland',
+	[COUNTRY_CZ]: 'Czech Republic',
+	[COUNTRY_SK]: 'Slovakia',
+	[COUNTRY_HU]: 'Hungary',
+	[COUNTRY_RO]: 'Romania',
+	[COUNTRY_BG]: 'Bulgaria',
+	[COUNTRY_GR]: 'Greece',
+
+	[COUNTRY_JP]: 'Japan',
+	[COUNTRY_CN]: 'China',
+	[COUNTRY_KR]: 'South Korea',
+	[COUNTRY_IN]: 'India',
+	[COUNTRY_SG]: 'Singapore',
+	[COUNTRY_HK]: 'Hong Kong',
+	[COUNTRY_TW]: 'Taiwan',
+	[COUNTRY_TH]: 'Thailand',
+	[COUNTRY_MY]: 'Malaysia',
+	[COUNTRY_ID]: 'Indonesia',
+	[COUNTRY_PH]: 'Philippines',
+	[COUNTRY_VN]: 'Vietnam',
+	[COUNTRY_AE]: 'United Arab Emirates',
+	[COUNTRY_SA]: 'Saudi Arabia',
+	[COUNTRY_IL]: 'Israel',
+};
