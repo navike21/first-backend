108b1043943597772c8fca9b24223ba020bd0fc5
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-03-28 04:31:50 -0500

    refactor: restructure server initialization and add graceful shutdown handling

---
+++ src/config/mainServer.ts
@@ -0,0 +1,46 @@
+import {
+	// connectToDatabase,
+	disconnectFromDatabase,
+} from '@Connection/connectionDB';
+import { Server } from 'node:http';
+import { Express } from 'express';
+import { logError, logInfo } from '@Helpers/log';
+import configEnvironment from '@Constants/environments';
+
+let server: Server;
+let isShuttingDown = false;
+
+export async function startServer(app: Express): Promise<void> {
+	try {
+		// await connectToDatabase();
+
+		// app.use(mainRouter());
+		// app.use(errorMiddleware);
+
+		server = app.listen(configEnvironment.PORT, () => {
+			logInfo(
+				`Server is running on port ${configEnvironment.PORT} in ${configEnvironment.PORT} mode.`,
+			);
+		});
+	} catch (error) {
+		logError(`Failed to start server: ${error as Error}`);
+		process.exit(1);
+	}
+}
+
+export async function handleServerShutdown(): Promise<void> {
+	if (isShuttingDown) return;
+	isShuttingDown = true;
+
+	logInfo('Shutting down server gracefully...');
+
+	server.close(async () => {
+		try {
+			await disconnectFromDatabase();
+			process.exit(0);
+		} catch (error) {
+			logError(`Error during server shutdown: ${error as Error}`);
+			process.exit(1);
+		}
+	});
+}
