8151eb7d27f0fb2f7652e7f235ac6b4775e7334c
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-03-28 06:30:58 -0500

    fix: comment out subscribers API import and usage in main router

--- src/routes/routes.ts
+++ src/routes/routes.ts
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 
 import { welcomeApi } from '@Modules/welcomeApi';
-import { subscribersApi } from '@Modules/subscribers';
+// import { subscribersApi } from '@Modules/subscribers';
 
 const router: Router = Router();
 
 const mainRouter = () => {
 	welcomeApi(router);
-	subscribersApi(router);
+	// subscribersApi(router);
 
 	return router;
 };
