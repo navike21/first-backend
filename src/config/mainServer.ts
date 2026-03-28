a4a23d5e5b6e374e8c65ac20a0f7212e7a0f508f
José Iván Chapoñán Casiano <ivan@navike21.com>
2026-03-28 04:58:39 -0500

    feat: implement server configuration in separate app.ts file and streamline server startup

--- src/config/mainServer.ts
+++ src/config/mainServer.ts
@@ -3,15 +3,18 @@ import {
 	disconnectFromDatabase,
 } from '@Connection/connectionDB';
 import { Server } from 'node:http';
-import { Express } from 'express';
+import type { Express } from 'express';
 import { logError, logInfo } from '@Helpers/log';
 import configEnvironment from '@Constants/environments';
+import { configApp } from './app';
 
 let server: Server;
 let isShuttingDown = false;
 
 export async function startServer(app: Express): Promise<void> {
 	try {
+		configApp(app);
+
 		await connectToDatabase();
 
 		// app.use(mainRouter());
--- src/index.ts
+++ src/index.ts
@@ -1,37 +1,7 @@
 import { handleServerShutdown, startServer } from '@Config/mainServer';
-import compression from 'compression';
-import cookieParser from 'cookie-parser';
-import helmet from 'helmet';
 import express from 'express';
-import { globalLimiter } from '@Config/limiter';
 
 const app = express();
-
-// Middleware
-app.use(express.json());
-
-app.use(express.urlencoded({ extended: true }));
-
-app.use(cookieParser());
-
-app.use(
-	compression({
-		threshold: 1024,
-	}),
-);
-
-app.use(
-	helmet({
-		contentSecurityPolicy: false,
-		crossOriginEmbedderPolicy: false,
-		frameguard: { action: 'deny' },
-		referrerPolicy: { policy: 'no-referrer' },
-		hidePoweredBy: true,
-		noSniff: true,
-	}),
-);
-
-app.use(globalLimiter);
 
 // Define your routes
 app.get('/', (req, res) => {
@@ -43,6 +13,7 @@ app.get('/api/health', (req, res) => {
 });
 
 startServer(app);
+
 process.on('SIGINT', handleServerShutdown);
 process.on('SIGTERM', handleServerShutdown);
 
